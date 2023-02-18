b8e612810ad703d9a820bddf532073cf851593a5
b1910075 <huyb1910075@student.ctu.edu.vn>
2023-02-19 01:30:59 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application."});
-});
+// app.get("/", (req, res) => {
+//     res.json({message: "Welcome to contact book application."});
+// });
 
 module.exports = app;
